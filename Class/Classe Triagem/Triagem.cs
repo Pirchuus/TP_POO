09dee888e25504c8d293b8c3f7a7e86b6005a402
anapintoo19 <anammpinto.fcp@gmail.com>
2023-10-31 14:35:24 +0000

    classe Triagem

--- Class/Classe Triagem/Triagem.cs
+++ Class/Classe Triagem/Triagem.cs
@@ -5,10 +5,23 @@ using System;
 /// </summary>
 public class Triagem
 {
+    #region ATRIBUTOS
+
+    public string Gravidade { get; set; } // pode ser um enumerado talvez
+    public string Sintomas { get; set; } // será que pode ser um enumerado ??????
+    public int Prioridade { get; set; }
+
+    #endregion
+
+    #region CONSTRUTOR
+
     public Triagem()
     {
         //
         // TODO: Add constructor logic here
         //
     }
+
+    #endregion
+    
 }
