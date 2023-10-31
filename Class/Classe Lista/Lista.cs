fbf9991cdc264deb92c877f17ed5af17369fd8b1
anapintoo19 <anammpinto.fcp@gmail.com>
2023-10-31 14:32:13 +0000

    classe Lista

--- Class/Classe Lista/Lista.cs
+++ Class/Classe Lista/Lista.cs
@@ -5,10 +5,23 @@
 /// </summary>
 public class Lista
 {
+    #region ATRIBUTOS
+
+	public int UtenteEspera { get; set; }
+	public int Prioridade { get; set; }
+
+    #endregion
+
+    #region CONSTRUTOR
+
 	public Lista()
 	{
 		//
 		// TODO: Add constructor logic here
 		//
 	}
+
+    #endregion
+
+   
 }
