c82a8f85278bf23cb107c72369fa3b4ee576f5aa
anapintoo19 <anammpinto.fcp@gmail.com>
2023-10-31 10:18:33 +0000

    classePessoa

--- Class/Classe Pessoa/Pessoa.cs
+++ Class/Classe Pessoa/Pessoa.cs
@@ -5,10 +5,28 @@ using System;
 /// </summary>
 public class Pessoa
 {
+    #region ATRIBUTOS
+
+    public string Nome { get; set; }
+    public int NumTelemovel { get; set; }
+    public string Genero { get; set; } // pode ser enumerado
+    public DateTime DataNascimento { get; set; }
+    public string Cidade { get; set; } // pode ser enumerado talvez
+    public int CodigoPostal { get; set; } // acho que não é do tipo int
+
+    #endregion
+
+    #region MÉTODOS
+    #endregion
+
+    #region CONSTRUTOR
     public Pessoa()
     {
         //
         // TODO: Add constructor logic here
         //
     }
+    #endregion
+
+    
 }
