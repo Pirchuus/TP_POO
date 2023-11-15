100c7963342139856f3ecd9656d710ae763eb848
Diogo Pinheiro <a24016@alunos.ipca.pt>
2023-11-15 17:42:13 +0000

    Program

--- TP_POO.App/Program.cs
+++ TP_POO.App/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_POO.Enums;
+using TP_POO.Class;
+using TP_POO.Dados;
+
+
+Console.WriteLine("Hello World!!");
